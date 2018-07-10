a51c781a6f50a6f42bc01b3d77c57e66eb9ee944
Pashqqqaaa <Pat.tyurin2017@yandex.ru>
2018-07-10 13:38:50 +0300

    six commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $('.fade').slick({
-  dots: true,
+  dots: false,
   infinite: true,
   speed: 500,
   fade: true,
-  cssEase: 'linear'
+  cssEase: 'linear',
+  arrows:true,
+  nextArrow: '<img src="img/2.2.png">',
+  prevArrow: '<img src="img/2.1.png>'	
 });
